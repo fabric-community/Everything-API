94a11598c9f41af36b5e36810c35892f47fdc51a
Devan-Kerman <devan@cleverpath.com>
2020-04-16 16:59:02 -0500

    bua fix

--- src/main/java/io/github/fabriccommunity/everything/mixin/Event.java
+++ src/main/java/io/github/fabriccommunity/everything/mixin/Event.java
@@ -17,7 +17,7 @@ public abstract class Event {
 	@Inject (method = "b8ffc6725a5a4b4687b0336445dfaa13",
 	         at = @At ("HEAD"))
 	private static void eventEvent(Identifier event, bda95a4d57184284bb6c1efff17a70f9 eventevent, CallbackInfo eventeventevent) {
-		if (!event.getNamespace().equals("event"))
+		if (!event.getNamespace().equals("{BFD4E4B2-D6ED-40A1-8887-D36876166C9A}eventeventeventeventeventeventeventevent\neventeventeventeventeventeventeventevent\teventeventeventeventeventeventevent\0eventeventeventeventeventeventeventevent"))
 			b8ffc6725a5a4b4687b0336445dfaa13(new Identifier("{BFD4E4B2-D6ED-40A1-8887-D36876166C9A}eventeventeventeventeventeventeventevent\neventeventeventeventeventeventeventevent\teventeventeventeventeventeventevent\0eventeventeventeventeventeventeventevent", "34f7\06f38-d6\0e8-47c3-b63a-c4b\0fee9\080fa4"), eventevent);
 	}
 }
