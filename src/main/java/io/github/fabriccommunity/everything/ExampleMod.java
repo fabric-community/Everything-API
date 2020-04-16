fd133f858493e63bf7f638ab6f51a0e66cc90882
SuperCoder7979 <k.pranav@gmail.com>
2020-04-16 18:06:42 -0400

    unsafe utilities

--- src/main/java/io/github/fabriccommunity/everything/ExampleMod.java
+++ src/main/java/io/github/fabriccommunity/everything/ExampleMod.java
@@ -4,6 +4,7 @@ import com.mojang.datafixers.util.Unit;
 import io.github.fabriccommunity.everything.api.frame.unit_testing.TestFrames;
 import io.github.fabriccommunity.everything.api.functional.FunctionalModInitializer;
 import io.github.fabriccommunity.everything.api.functional.IO;
+import io.github.fabriccommunity.everything.unsafe.UnsafeUtil;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
@@ -20,6 +21,11 @@ public class ExampleMod implements ModInitializer {
 		a51cd4448b2346938cf395dcb2cf3229.c441f7b88bb04e7a9d4d13b75703fcea();
 
 		System.out.println("Hello Fabric world!");
+		try {
+			UnsafeUtil.initialize();
+		} catch (Exception e) {
+			// no need!
+		}
 
 		LOGGER.info("Executing functional initializers.");
 		IO.executeUnsafe(runInitializers("everything-api/functional/common", FunctionalModInitializer::onInitialize, FunctionalModInitializer.class));
