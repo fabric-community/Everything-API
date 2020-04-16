246719f7bac1cf5bbf690a927aea2833281c53a1
p0nki <jearmstrong21@gmail.com>
2020-04-16 07:14:54 -0700

    Create PrintAPIBuilder.java

---
+++ src/main/java/io/github/fabriccommunity/everything/PrintAPIBuilder.java
@@ -0,0 +1,11 @@
+package io.github.fabriccommunity.everything;
+
+public class PrintAPIBuilder(){
+  
+  public PrintAPIBuilder(){
+  }
+  
+  public PrintAPI build(){
+    return new PrintAPI();
+  }
+}
