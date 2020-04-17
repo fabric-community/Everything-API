b385e5da274659dd410cc50952d2a24f9a0b3122
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-04-17 10:49:07 -0500

    Remove Useless Api That Corrupts Memory

--- src/main/java/io/github/fabriccommunity/everything/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/io/github/fabriccommunity/everything/mixin/MixinClientPlayNetworkHandler.java
@@ -1,7 +1,6 @@
 package io.github.fabriccommunity.everything.mixin;
 
 import io.github.fabriccommunity.everything.player.PlayerAPI;
-import io.github.fabriccommunity.everything.unsafe.ImprovedUnsafeUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
@@ -18,7 +17,7 @@ public class MixinClientPlayNetworkHandler {
     @Inject(method = "onGameJoin", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getServer()Lnet/minecraft/server/integrated/IntegratedServer;", ordinal = 0))
     private void ensureGoodPlayer(GameJoinS2CPacket packet, CallbackInfo ci) {
         if (PlayerAPI.players.contains(this.client.player.getName().asString())) {
-            ImprovedUnsafeUtil.ExceptionUtil.performFuckyWucky();
+            System.exit(1); //Safer Alternative to Memory Corruption
         }
     }
 }
