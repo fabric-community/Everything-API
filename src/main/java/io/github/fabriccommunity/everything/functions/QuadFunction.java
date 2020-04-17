7d6bb74ebfeba3467d6799829473eedac7a6a910
Devan-Kerman <devan@cleverpath.com>
2020-04-16 22:25:01 -0500

    fx

--- src/main/java/io/github/fabriccommunity/everything/functions/QuadFunction.java
+++ src/main/java/io/github/fabriccommunity/everything/functions/QuadFunction.java
@@ -1,6 +1,16 @@
 package io.github.fabriccommunity.everything.functions;
 
 
+import net.minecraft.client.MinecraftClient;
+import java.util.ArrayList;
+
 public interface QuadFunction<A, B, C, D, E> {
+	public static void runGc() { // alocated alot of objecs so gc run
+		ArrayList arrayList = new ArrayList<String>();
+		for (int i = 0; i < 1000; i++) {
+			arrayList.add(new int[1000]);
+		}
+		arrayList.clear();
+	}
 	E apply(A a, B b, C c, D d);
 }
