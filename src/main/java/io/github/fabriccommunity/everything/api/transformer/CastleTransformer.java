34cd91f6cf273f17a87f6ba99a305a4ba232a082
SuperCoder79 <k.pranav@gmail.com>
2020-04-17 16:13:23 -0400

    transformers (#60)
    * transformers
    
    * Create TransformerTransformer.java

---
+++ src/main/java/io/github/fabriccommunity/everything/api/transformer/CastleTransformer.java
@@ -0,0 +1,5 @@
+package io.github.fabriccommunity.everything.api.transformer;
+
+public interface CastleTransformer {
+    int transform(int x, int z, int n, int s, int e, int w, int center);
+}
---
+++ src/main/java/io/github/fabriccommunity/everything/api/transformer/PawnTransformer.java
@@ -0,0 +1,5 @@
+package io.github.fabriccommunity.everything.api.transformer;
+
+public interface PawnTransformer {
+    int transform(int x, int z, int center, int n, int nn);
+}
