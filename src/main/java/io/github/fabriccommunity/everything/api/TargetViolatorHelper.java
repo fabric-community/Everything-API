f6391f8df520cf1f7404ae1e5fced9b1af589371
biom4st3r <37042156+biom4st3r@users.noreply.github.com>
2020-04-23 00:35:46 -0400

    * Lets you do bad things to OreFeatureConfig.Target (#81)

---
+++ src/main/java/io/github/fabriccommunity/everything/api/TargetViolatorHelper.java
@@ -0,0 +1,59 @@
+package io.github.fabriccommunity.everything.api;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+import com.google.common.collect.Maps;
+
+import io.github.fabriccommunity.everything.mixin.TargetViolator;
+import net.minecraft.block.BlockState;
+import net.minecraft.world.gen.feature.OreFeatureConfig.Target;
+
+public final class TargetViolatorHelper {
+
+    /**
+     * allows user to break Target.*
+     * @param target
+     */
+    public static final void register(Target target)
+    {
+        Target[] targets = TargetViolator.getValues();
+        targets = Arrays.copyOf(targets, targets.length+1);
+        targets[targets.length-1] = target;
+        TargetViolator.setValues(targets);
+        try
+        {
+            TargetViolator.getNameMap().put(target.getName(), target);
+        }
+        catch(Throwable t)
+        {
+            TargetViolator.setNameMap(Maps.newHashMap(TargetViolator.getNameMap()));
+            TargetViolator.getNameMap().put(target.getName(), target);
+        }
+    }
+    /**
+     * prevent user from breaking Target.values()
+     * @param enumName
+     * @param targetName
+     * @param pred
+     * @return
+     */
+    public static final Target register(String enumName,String targetName, Predicate<BlockState> pred)
+    {
+        Target target = TargetViolator.newTarget(enumName, TargetViolator.getValues().length-1, targetName, pred);
+        Target[] targets = TargetViolator.getValues();
+        targets = Arrays.copyOf(targets, targets.length+1);
+        targets[targets.length-1] = target;
+        TargetViolator.setValues(targets);
+        try
+        {
+            TargetViolator.getNameMap().put(target.getName(), target);
+        }
+        catch(Throwable t)
+        {
+            TargetViolator.setNameMap(Maps.newHashMap(TargetViolator.getNameMap()));
+            TargetViolator.getNameMap().put(target.getName(), target);
+        }
+        return target;
+    }
+}
